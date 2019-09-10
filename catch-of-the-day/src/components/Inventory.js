2f286a692137eee6c17d8a3b6f25c62019bd6267
Seth Schwartz <seth.schwartz@thoughtworks.com>
2019-09-10 11:36:27 -0500

    Completes 7

---
+++ catch-of-the-day/src/components/Inventory.js
@@ -0,0 +1,10 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+
+class Inventory extends React.Component {
+  render() {
+    return <div className="inventory">Inventory!!!</div>;
+  }
+}
+
+export default Inventory;
